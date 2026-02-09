c7f03fad8599d68e2c23d4209e9ee94c9b5556bb
Igor <contato.igorgabriel@hotmail.com>
2026-02-09 10:38:53 -0300

    add SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import {
     CartHydrator,
     Footer,
@@ -13,9 +14,22 @@ import { Children } from "~types";
 import { poppins } from "~utils";
 import "~styles";
 
+export const metadata: Metadata = {
+    title: "Starsoft",
+    description:
+        "Explore e compre NFTs em nosso marketplace. Descubra coleções exclusivas, veja detalhes de cada item e gerencie seu carrinho de compras de forma fácil e intuitiva.",
+    keywords: ["NFT", "marketplace", "comprar NFTs", "coleções digitais"],
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false
+    },
+    authors: [{ name: "Igor Gabriel Vieira Chaves" }]
+};
+
 export default function RootLayout({ children }: Children) {
     return (
-        <html>
+        <html lang="en">
             <body className={poppins.className}>
                 <ViewportContextProvider>
                     <PageContainer>
